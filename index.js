e6c5ac59a63869f8097865ece475b86c62ff60a0
MohamadYazbak <mohamadyaz1999@hotmail.com>
2024-10-02 20:38:38 +0300

    Your commit message here

---
+++ index.js
@@ -0,0 +1,16 @@
+document.getElementById("red").onclick = function() {
+    window.location.href = "red.html";
+};
+
+document.getElementById("blue").onclick = function() {
+    window.location.href = "blue.html";
+};
+
+document.getElementById("yellow").onclick = function() {
+    window.location.href = "yellow.html";
+};
+
+document.getElementById("green").onclick = function() {
+    window.location.href = "green.html";
+};
+
